74f985f75a5e299f8fe592372dc9c3696fe4a370
sutton <50450947+Ioome@users.noreply.github.com>
2023-04-28 18:27:35 +0800

    Update MyServerHandler.java
    Co-Authored-By: Meg Sutton <6217841+sutton@users.noreply.github.com>

--- src/main/java/com/farm/data/nio/server/MyServerHandler.java
+++ src/main/java/com/farm/data/nio/server/MyServerHandler.java
@@ -21,9 +21,10 @@ import java.util.Date;
  * @date: 2023-04-24 09:48
  */
 
-@Slf4j
 public class MyServerHandler extends ChannelInboundHandlerAdapter {
 
+
+    private final Logger logger = LoggerFactory.getLogger(MyServerHandler.class);
     @Resource
     private FarmEquipmentService farmEquipmentService;
 
@@ -34,11 +35,11 @@ public class MyServerHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelActive (ChannelHandlerContext ctx) throws Exception {
         SocketChannel channel = (SocketChannel) ctx.channel();
-        log.info("链接报告开始");
-        log.info("链接报告信息：有一客户端链接到本服务端");
-        log.info("链接报告IP:{}", channel.localAddress().getHostString());
-        log.info("链接报告Port:{}", channel.localAddress().getPort());
-        log.info("链接报告完毕");
+        logger.info("链接报告开始");
+        logger.info("链接报告信息：有一客户端链接到本服务端");
+        logger.info("链接报告IP:{}", channel.localAddress().getHostString());
+        logger.info("链接报告Port:{}", channel.localAddress().getPort());
+        logger.info("链接报告完毕");
         String str = "通知客户端链接建立成功" + " " + new Date() + " " + channel.localAddress().getHostString() + "\r\n";
         ctx.writeAndFlush(str);
     }
@@ -48,27 +49,27 @@ public class MyServerHandler extends ChannelInboundHandlerAdapter {
      */
     @Override
     public void channelInactive (ChannelHandlerContext ctx) throws Exception {
-        log.info("客户端断开链接{}", ctx.channel().localAddress());
+        logger.info("客户端断开链接{}", ctx.channel().localAddress());
     }
 
     @Override
     public void channelRead (ChannelHandlerContext ctx, Object msg) throws Exception {
         String format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
-        log.info("date {}", format);
+        logger.info("date {}", format);
         //获取客户端ip
         InetSocketAddress ipSocket = (InetSocketAddress) ctx.channel().remoteAddress();
         String clientIp = ipSocket.getAddress().getHostAddress();
-        log.info("客户端ip: {}", clientIp);
+        logger.info("客户端ip: {}", clientIp);
 
         //获取客户端端口
         int clientPort = ipSocket.getPort();
-        log.info("客户端端口: {}", clientPort);
+        logger.info("客户端端口: {}", clientPort);
 
         //获取客户端信息
-        log.info("服务端收到: {}", msg);
+        logger.info("服务端收到: {}", msg);
 
         //获取通道 id
-        log.info("通道 id: {}", ctx.channel().id());
+        logger.info("通道 id: {}", ctx.channel().id());
 
 
         String str = "服务端收到: " + new Date() + " " + msg + "\r\n";

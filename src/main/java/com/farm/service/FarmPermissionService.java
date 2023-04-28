9188d78510d3e523aa376ac0ac52384e1433a135
sutton <50450947+Ioome@users.noreply.github.com>
2023-04-28 16:57:45 +0800

    sutton
    设置基础结构

---
+++ src/main/java/com/farm/service/FarmPermissionService.java
@@ -0,0 +1,16 @@
+package com.farm.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.farm.entity.po.FarmEquipment;
+import com.farm.entity.po.FarmPermission;
+import org.springframework.stereotype.Service;
+
+/**
+ * @name: FarmPermission
+ * @author: sutton
+ * @date: 2023-04-28 16:54
+ * @description: FarmPermission
+ */
+@Service
+public interface FarmPermissionService extends IService<FarmPermission> {
+}
---
+++ src/main/java/com/farm/service/impl/FarmPermissionServiceImpl.java
@@ -0,0 +1,16 @@
+package com.farm.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.farm.entity.po.FarmPermission;
+import com.farm.mapper.FarmPermissionMapper;
+import com.farm.service.FarmPermissionService;
+
+/**
+ * @name: FarmPermissionServiceImpl
+ * @author: sutton
+ * @date: 2023-04-28 16:56
+ * @description: FarmPermissionServiceImpl
+ */
+
+public class FarmPermissionServiceImpl extends ServiceImpl<FarmPermissionMapper, FarmPermission> implements FarmPermissionService {
+}

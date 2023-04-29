8a299e7100dfa8330799ba1c85e9058ea0d25039
sutton <50450947+Ioome@users.noreply.github.com>
2023-04-29 17:53:50 +0800

    Update FarmEquipmentServiceImpl.java

--- src/main/java/com/farm/service/impl/FarmEquipmentServiceImpl.java
+++ src/main/java/com/farm/service/impl/FarmEquipmentServiceImpl.java
@@ -7,9 +7,9 @@ import com.farm.service.FarmEquipmentService;
 import io.netty.channel.ChannelHandlerContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 /**
  * @name: FarmEquipmentServiceImpl
@@ -17,6 +17,7 @@ import java.util.List;
  * @date: 2023-04-28 16:52
  * @description: 硬件设备管理
  */
+@Service
 public class FarmEquipmentServiceImpl extends ServiceImpl<FarmEquipmentMapper, FarmEquipment> implements FarmEquipmentService {
 
 
@@ -33,8 +34,6 @@ public class FarmEquipmentServiceImpl extends ServiceImpl<FarmEquipmentMapper, F
      */
     @Override
     public void saveEquipment (ChannelHandlerContext ctx, Object msg) {
-        // TODO 先获取到设备的信息， 如果设备不存在于数据库，那么将设备通道 id 管理，并且将设备信息保存到数据库，如果存在则直接更改
-        // TODO 设备的状态，然后将设备的信息保存到数据库
         logger.info("收到的消息为: {}", ctx);
         logger.info("收到客户端的消息:{}", msg.toString());
     }

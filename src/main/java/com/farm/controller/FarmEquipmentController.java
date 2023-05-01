6060d8934b83f0ee7f5ff7f5dad399e34daac489
sutton <50450947+Ioome@users.noreply.github.com>
2023-05-01 15:55:06 +0800

    设备

---
+++ src/main/java/com/farm/controller/FarmEquipmentController.java
@@ -0,0 +1,19 @@
+package com.farm.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author sutton
+ * @description 设备controller层
+ * @date 2023-05-01
+ */
+@Slf4j
+@RestController
+@RequestMapping("/equipment")
+public class FarmEquipmentController {
+}
+
+
+

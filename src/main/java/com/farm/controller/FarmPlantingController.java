9c5c2e3978d3e4e32608770a295a03064630831b
sutton <50450947+Ioome@users.noreply.github.com>
2023-05-01 15:17:06 +0800

    Update FarmPlantingController.java

--- src/main/java/com/farm/controller/FarmPlantingController.java
+++ src/main/java/com/farm/controller/FarmPlantingController.java
@@ -1,24 +1,8 @@
 package com.farm.controller;
 
-import com.alibaba.fastjson.JSON;
-import com.farm.entity.po.FarmPlanting;
-import com.farm.mapper.FarmPlantingMapper;
-import com.farm.utils.ResponseResult;
-import www.itgongju.com.entity.FarmPlanting;
-import www.itgongju.com.mapper.FarmPlantingMapper;
-import www.itgongju.com.util.ReturnT;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import org.springframework.web.servlet.ModelAndView;
-
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author www.itgongju.com

7b449a11f55a95cd8c5752883269803f554476b2
sutton <50450947+Ioome@users.noreply.github.com>
2023-05-10 16:40:55 +0800

    Update FieldAnnotationUtil.java

--- src/main/java/com/farm/utils/FieldAnnotationUtil.java
+++ src/main/java/com/farm/utils/FieldAnnotationUtil.java
@@ -30,4 +30,23 @@ public class FieldAnnotationUtil {
             field.set(entity, newAnnotations);
         }
     }
+
+    /**
+     * 在实体类上添加注解
+     * @param entity 实体类对象
+     * @param annotationClass 要添加的注解的类型
+     */
+    public static void addAnnotation(Object entity, Class<? extends Annotation> annotationClass) throws NoSuchFieldException, IllegalAccessException, InstantiationException {
+        Class<?> clazz = entity.getClass();
+        Annotation annotation = clazz.getAnnotation(annotationClass);
+        if (annotation == null) {
+            Field field = Class.class.getDeclaredField("annotations");
+            field.setAccessible(true);
+            Annotation[] annotations = (Annotation[]) field.get(clazz);
+            Annotation[] newAnnotations = new Annotation[annotations.length + 1];
+            System.arraycopy(annotations, 0, newAnnotations, 0, annotations.length);
+            newAnnotations[newAnnotations.length - 1] = annotationClass.newInstance();
+            field.set(clazz, newAnnotations);
+        }
+    }
 }

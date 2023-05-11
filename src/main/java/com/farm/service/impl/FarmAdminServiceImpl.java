1dad554c8d61edaab583f12af3260ff4d99a98a7
sutton <50450947+Ioome@users.noreply.github.com>
2023-05-11 14:20:11 +0800

    增加加密解密

--- src/main/java/com/farm/service/impl/FarmAdminServiceImpl.java
+++ src/main/java/com/farm/service/impl/FarmAdminServiceImpl.java
@@ -1,5 +1,6 @@
 package com.farm.service.impl;
 
+import cn.hutool.core.text.CharSequenceUtil;
 import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
@@ -73,6 +74,8 @@ public class FarmAdminServiceImpl extends ServiceImpl<FarmAdminMapper, FarmAdmin
      */
     @Override
     public FarmAdmin register (FarmAdmin umsAdminParam) {
+
+        String finalPassWord = RSAUtil.decryptBase64(umsAdminParam.getPassword().trim());
         FarmAdmin farmAdmin = new FarmAdmin();
         BeanUtils.copyProperties(umsAdminParam, farmAdmin);
         farmAdmin.setCreatedTime(new Date());
@@ -81,7 +84,7 @@ public class FarmAdminServiceImpl extends ServiceImpl<FarmAdminMapper, FarmAdmin
         if (!umsAdminList.isEmpty()) {
             throw new MyException(FarmExceptionEnum.NAME_EXISTED.getCode(), "该用户名已存在");
         }
-        String encodePassword = passwordEncoder.encode(farmAdmin.getPassword());
+        String encodePassword = passwordEncoder.encode(finalPassWord);
         farmAdmin.setPassword(encodePassword);
         adminMapper.insert(farmAdmin);
         return umsAdminParam;
@@ -98,8 +101,12 @@ public class FarmAdminServiceImpl extends ServiceImpl<FarmAdminMapper, FarmAdmin
     public String login (FarmAdmin farmAdmin) {
         long start = System.currentTimeMillis();
         LOGGER.info("用户开始登录............");
+        if (StrUtil.isBlank(farmAdmin.getPassword().trim())) {
+            throw new MyException("密码为空，请重新输入密码");
+        }
+        String finalPassWord = RSAUtil.decryptBase64(farmAdmin.getPassword().trim());
         //记录秒数
-        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(farmAdmin.getUsername(), farmAdmin.getPassword());
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(farmAdmin.getUsername(), finalPassWord);
         Authentication authenticate = authenticationManager.authenticate(authenticationToken);
         if (isNull(authenticate)) {
             throw new MyException("账户或密码错误");
@@ -178,16 +185,19 @@ public class FarmAdminServiceImpl extends ServiceImpl<FarmAdminMapper, FarmAdmin
     @Override
     public void updatePassword (FarmPassWordVo farmPassWordVo) {
         Long userId = UserInfoUtils.getUserId();
-        if (farmPassWordVo == null || StrUtil.isBlank(farmPassWordVo.getOldPassword()) || StrUtil.isBlank(farmPassWordVo.getNewPassword())) {
+        String oldP = RSAUtil.decryptBase64(farmPassWordVo.getOldPassword().trim());
+        String newP = RSAUtil.decryptBase64(farmPassWordVo.getNewPassword().trim());
+        if (CharSequenceUtil.isBlank(oldP) || CharSequenceUtil.isBlank(newP)) {
             throw new MyException("旧密码或新密码为空");
         }
+
         FarmAdmin farmAdmin = adminMapper.selectOne(new QueryWrapper<FarmAdmin>().lambda().eq(FarmAdmin::getId, userId));
-        boolean matches = passwordEncoder.matches(farmPassWordVo.getOldPassword(), farmAdmin.getPassword());
+        boolean matches = passwordEncoder.matches(oldP, farmAdmin.getPassword());
         if (!matches) {
             LOGGER.info("旧密码错误");
             throw new MyException(FarmExceptionEnum.WRONG_PASSWORD.getCode(), FarmExceptionEnum.WRONG_PASSWORD.getMessage());
         }
-        String encode = passwordEncoder.encode(farmPassWordVo.getNewPassword());
+        String encode = passwordEncoder.encode(newP);
         LOGGER.info(encode);
         int update = adminMapper.update(null, new UpdateWrapper<FarmAdmin>().lambda().eq(FarmAdmin::getId, userId).set(FarmAdmin::getPassword, encode));
         if (update <= 0) {

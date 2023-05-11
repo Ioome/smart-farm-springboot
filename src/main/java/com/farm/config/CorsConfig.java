927399e5324aac5d6d307a8e90e93404ef5e03c2
sutton <50450947+Ioome@users.noreply.github.com>
2023-05-11 20:58:40 +0800

    Update CorsConfig.java

--- src/main/java/com/farm/config/CorsConfig.java
+++ src/main/java/com/farm/config/CorsConfig.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
@@ -15,7 +17,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * 跨域问题
  */
 @Configuration
-@EnableWebMvc
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsConfig implements WebMvcConfigurer {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(CorsConfig.class);
